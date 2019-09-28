e4e161b56bb0ca9900d1a812f37baea67c2356d8
J Tsai <jtsai972@gmail.com>
2019-09-28 00:50:59 -0500

    setting up the API query and a document ready function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,60 @@
 var topics = ["puppies", "kitties", "bunnies", "ducklings", "puppy", "kitty", "hamster", "corgis", "dogs", "cats", "red pandas", "cute", "cute animals", "baby animals", "adorable animals"];
-
-var myKey = config.giphyKey; //api key
+//
 var str;
 
+//API variables
+var myKey = config.giphyKey; //api key
 
+// ToDo: Properly name and set these up, these are placeholders
+
+var queryValue = ""; //input
+var printNum = 10 //
+
+//This needs to go after the terms in it
+var queryURL = "https://api.giphy.com/v1/gifs/search?q" + queryValue + "&api_key" + myKey;
+
+
+//generating topics at the start
 for(let i = 0; i < topics.length; i++) {
     buttonGen(topics[i]);
 }
 
+//Do this after page loads
+$(document).ready(function () {
+    
+
+    
+});
+
+
+
+// ? Ajax incoming
+$.ajax({
+    url : queryURL,
+    method : "GET"
+}).then( function() {
+
+});
+
+// ? Welcome to functions
 
 function buttonGen(str) {
     var newButton = $("<button>").text(str);
     $("#buttons").append(newButton);
 }
 
+function imageGen() {
+    $("<figure>").append(
+        $("<img>").attr({
+            //! these are temporary variables for setup
+            "src" : queryResult,
+            "data-value": "still",
+            "data-still" : thumbNail,
+            "data-animate" : searchResult
+        }),
+        $("<figcaption>").text(`Content rating: ${tempRating}`)
+    );
+}
 
 // // ToDo: Create an array of starter topics
 
@@ -23,7 +64,7 @@ function buttonGen(str) {
 // ToDo: Create a variable for the api url and search topic
 
 // Functions 
-// ToDo: make something to generate and append the buttons
+// // ToDo: make something to generate and append the buttons
 
 // ToDo: print 10 gif thumbnails to the page
 

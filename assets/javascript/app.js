d515c57dc383517a4c43424eca3ff33724eb46ec
J Tsai <jtsai972@gmail.com>
2019-09-28 11:43:22 -0500

    setting up some of the queries

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var topics = ["puppies", "kitties", "bunnies", "ducklings", "puppy", "kitty", "hamster", "corgis", "dogs", "cats", "red pandas", "cute", "cute animals", "baby animals", "adorable animals"];
 //
-var str;
+var str, imgStill, imgAnimate;
 
 //API variables
 var myKey = config.giphyKey; //api key
@@ -8,37 +8,48 @@ var myKey = config.giphyKey; //api key
 // ToDo: Properly name and set these up, these are placeholders
 
 var queryValue = ""; //input
-var printNum = 10 //
+var queryLimit = 10 //
+var queryRating = "G";
 
 //This needs to go after the terms in it
-var queryURL = "https://api.giphy.com/v1/gifs/search?q" + queryValue + "&api_key" + myKey;
-
+var queryURL = 
+    "https://api.giphy.com/v1/gifs/search?" +
+    "&api_key" + myKey + 
+    "&limit=" + queryLimit + 
+    "&rating=" + queryRating + 
+    "&offset=0&lang=en" + 
+    "&q=" + queryValue;
 
 //generating topics at the start
 for(let i = 0; i < topics.length; i++) {
     buttonGen(topics[i]);
 }
 
+
+// var img = $(this);
+// var state = img.attr("data-state");
+
+// state = (state === "still") ?
+// 	img.attr({src : img.attr("data-animate"), "data-state" : "animate"}) :
+// 	img.attr({src : img.attr("data-still", "data-state" : "still")})
+
 //Do this after page loads
 $(document).ready(function () {
     $("#buttons").on("click", "button", function() {
 
-        // ? Ajax incoming
-        $.ajax({
-            url : queryURL,
-            method : "GET"
-        }).then( function() {
+        //get button value, trim it, and then send put it into the ajax
 
-        });
+        
     });
-
-
 });
 
 // ? Welcome to functions
 
 function buttonGen(str) {
-    var newButton = $("<button>").text(str);
+    var newButton = $("<button>")
+        .text(str)
+        .attr("data-query", (str.replace(/\s/g,'/')));
+
     $("#buttons").append(newButton);
 }
 
@@ -46,13 +57,27 @@ function imageGen() {
     $("<figure>").append(
         $("<img>").attr({
             //! these are temporary variables for setup
-            "src" : queryResult,
+            "src" : imgStill,
             "data-value": "still",
-            "data-still" : thumbNail,
-            "data-animate" : searchResult
+            "data-still" : imgStill,
+            "data-animate" : imgAnimate
         }),
         $("<figcaption>").text(`Content rating: ${tempRating}`)
     );
+}
+
+function queryAPI() {
+    // ? Ajax incoming
+    $.ajax({
+        url : queryURL,
+        method : "GET"
+    }).then( function(response) {
+        var queryResult = response;
+
+        for(let i = 0; i < )
+        imgStill = response.data[i].images.original_still.url;
+
+    });
 }
 
 // // ToDo: Create an array of starter topics
